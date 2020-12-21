81bc570f81157d21fad3f844b9b21112b76a7d53
Eduardo Vinicius <eduardo.vinicius@alianca.com.br>
2020-12-20 22:16:59 -0300

    Adicionado regra de negocio

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid, v4, validate } = require('uuid');
 
 const app = express();
 
@@ -11,23 +11,76 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+
+  return response.json(repositories);
+
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = { id: uuid(), title, url, techs, likes: 0 };
+
+  /*if (!isUuid(repository.id)) {
+    return response.status(404).json({ error: 'Invalid ID' });
+  }*/
+
+  repositories.push(repository);
+
+  return response.json(repository);
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositorieIndex = repositories.findIndex(x => x.id === id);
+
+  if (repositorieIndex === -1) {
+    return response.status(400).json({ error: 'Repository Invalid' })
+  }
+
+  const repositorie = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[repositorieIndex].likes,
+  };
+
+  repositories[repositorieIndex] = repositorie;
+
+  return response.json(repositorie);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositorieIndex = repositories.findIndex(x => x.id === id);
+
+  if (repositorieIndex >= 0) {
+    repositories.splice(repositorieIndex, 1);
+  } else {
+    return response.status(400).json({ error: 'Repository Invalid' })
+  }
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositorieIndex = repositories.findIndex(x => x.id === id);
+
+  if (repositorieIndex === -1) {
+    return response.status(400).json({ error: 'repositorie not found.' });
+  }
+
+  repositories[repositorieIndex].likes++;
+
+  return response.json(repositories[repositorieIndex]);
+
 });
 
 module.exports = app;
